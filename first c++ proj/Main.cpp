7d29ea80d996ef42281262ea91adbc5e1152d8e8
UL7RAGAMER <siddkum@outlook.com>
2024-06-28 15:09:45 +0530

    first commit '

---
+++ first c++ proj/Main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define log(x) cout << x << endl;
+struct node {
+	int value{};
+	node* ptr{};
+	node(int v, node* p) 
+	{ 
+		value = v;
+		ptr = p;
+	}
+	node() {};
+	friend ostream& operator<<(ostream& os, const node obj) {
+		os << obj.value;
+ 		if (obj.ptr) {
+			os << " -> " << *obj.ptr;
+		}
+		else {
+			os << " -> nullptr";
+		}
+		return os;
+	}
+
+};
+class ll {
+
+	node* head{};
+	node* current{};
+	node* x{};
+public:
+	~ll() 
+	{
+		node* c = head;
+		while (c->ptr) {
+			node* next = c->ptr;
+			delete c;
+			c = next;
+		}
+	}
+
+	void append(int v)
+	{
+		if (!head) 
+		{
+			node* n= new node(v, nullptr);
+			head = n;
+			current = head;
+			return;
+		}
+		node* n = new node(v, nullptr);
+		x = n;
+		current->ptr = n;
+		current = n;
+	}
+	int get(int index){
+		int v = -1;
+		node* c = head;
+		
+		for (int i=0;i <= index;i++) 
+		{
+			v = c->value;
+			c = c->ptr;
+		}
+		return v;
+	}
+	int pop(int index)
+	{
+		node* c = head;
+		if (index != 0) 
+		{	
+			node* pp=nullptr;
+			for (int i = 0;i < index;i++)
+			{
+				if (index - 1 == i)
+				{
+					pp = c;
+				}
+				c = c->ptr;
+				
+			}
+			int v = c->value;
+			node* np = c->ptr;
+			pp->ptr = np;
+			delete c;
+			return v;
+		}
+		else
+		{
+			int v = head->value;
+			node *np = head->ptr;
+			delete head;
+			head = np;
+			return v;
+		}
+				
+	}
+	void print()
+	{
+		log(*head);
+	}
+
+	ll operator+(ll& n )
+	{
+		ll* sum = new ll;
+		current->ptr = n.head;
+		sum->head = head;
+		sum->current = n.current;
+		return *sum;
+	}
+
+
+};
+
+//*((int*)((char*)pointer_array + byte_location))= new_val;
+int main()
+{
+	ll a;
+	a.append(1);
+	a.append(2);
+	a.append(3);
+	a.print();
+	a.pop(0);
+	a.print();
+
+}
+	
---
+++ first c++ proj/log.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int asd = 1;
+
+inline void Log(void* mes) 
+{
+	
+	std::cout << mes << std::endl;
+}
