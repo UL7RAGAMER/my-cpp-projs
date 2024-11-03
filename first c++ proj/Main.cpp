1d8ac8e9278c46a3fc79887ea2f8cc3fb9322833
UL7RA GAMER <siddkum@outlook.com>
2024-11-03 09:31:37 +0530

    Implemented copy and move semantics

--- first c++ proj/Main.cpp
+++ first c++ proj/Main.cpp
@@ -33,6 +33,26 @@ class unordered_map
 private:
 	int bucket_size;
 	vector<vector<pair<K, V>>> buckets;
+	int elements = 0;
+	const double load_factor = 0.85;
+	void rehash()
+	{
+		log("rehash");
+		int new_bucket_size = bucket_size * 2;
+		vector<vector<pair<K, V>>> new_bucket(new_bucket_size) ;
+		for (const auto& b : buckets)
+		{
+			for (const auto& p : b)
+
+			{
+
+				int new_hash_index = Hash(p.first, new_bucket_size);
+				new_bucket[new_hash_index].emplace_back(p);
+			}
+		}
+		buckets = move(new_bucket);
+		bucket_size = new_bucket_size;
+	}
 
 
 
@@ -108,7 +128,8 @@ public:
 		for (auto it = bucket.begin(); it != bucket.end(); ++it) 
 		{
 			if (it->first == key)
-			{
+			{	
+				elements--;
 				bucket.erase(it);
 				return;
 			}
@@ -125,9 +146,14 @@ public:
 				return p.second;
 			}
 		}
-
+		elements++;
 		buckets[hashIndex].emplace_back(key, V());
+		if ((elements / bucket_size) > load_factor)
+			rehash();
+		
 		return buckets[hashIndex].back().second;
+
+
 	}
 
 	Iterator begin() {
@@ -153,6 +179,25 @@ public:
 		}
 		return end();
 	}
+
+	unordered_map& operator=(unordered_map& o) noexcept
+	{
+		log("Copy");
+		buckets.clear();
+		buckets = o.buckets;
+		bucket_size = o.bucket_size;
+		return *this;
+	}
+	unordered_map& operator=(unordered_map&& o) noexcept
+	{
+		log("Moved");
+		buckets.clear();
+		buckets = o.buckets;
+		bucket_size = o.bucket_size;
+		o.buckets.clear();
+		o.bucket_size = 0;
+		return *this;
+	}
 };
 
 
@@ -162,14 +207,19 @@ int main()
 {	
 	unordered_map<int, int> o{};
 	log(Hash(321,10));
-	log(Hash(133,10));
 	o[133] = 1;
 	o[321] = 2;
-	
-	o.del(133);
-	for (auto& p : o)
-	{
-		log(p.first);
-	}
+	o[113] = 1;
+	o[321] = 2;
+	o[1323] = 1;
+	o[3214] = 2;
+	o[1334] = 1;
+	o[3252] = 2;
+	o[123] = 1;
+	o[322341] = 2;
+	o[1333] = 1;
+	log(Hash(133, 10));
+	o[3251] = 2;
+
 
 }
